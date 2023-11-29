43ec2de8010433dc42e2bc8a0cc878975758022c
sb-boomi <151708314+sb-boomi@users.noreply.github.com>
2023-11-29 14:25:07 +1100

    Update updateExt.sh

--- boomicicd-cli/cli/scripts/updateExt.sh
+++ boomicicd-cli/cli/scripts/updateExt.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+set -a
+
+source $WD/bin/common.sh
+source $WD/bin/propertiesCICD.sh
+
+ARGUMENTS=(authToken componentId packageVersion notes componentType env listenerStatus)
+
+inputs "$@"
+if [ "$?" -gt 0 ]
+then
+	exit $?
+fi
+
+$WD/bin/updateExtensions.sh componentId=$componentId packageVersion=$packageVersion componentType=$componentType notes="$notes" env=$env listenerStatus=$listenerStatus
