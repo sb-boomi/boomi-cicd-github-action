b9bb06bf073997d4e814a46f398a963b56b51940
sb-boomi <151708314+sb-boomi@users.noreply.github.com>
2023-12-04 16:22:32 +1100

    Update executeProc.sh

--- boomicicd-cli/cli/scripts/executeProc.sh
+++ boomicicd-cli/cli/scripts/executeProc.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+set -a
+
+source $WD/bin/common.sh
+source $WD/bin/propertiesCICD.sh
+
+ARGUMENTS=(authToken extensionJson env)
+
+inputs "$@"
+if [ "$?" -gt 0 ]
+then
+	exit $?
+fi
+
+$WD/bin/updateExtensions.sh extensionJson=$extensionJson env=$env
